aa3f130da6a0658044b8e87ddfb295949c8efd69
Lucas Barros <lucas.barros@mindsdigital.net>
2023-10-11 21:37:12 -0300

    modificações na aula e escrita da documentação

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -2,18 +2,28 @@ public class Vaga {
 
 	private String id;
 	private boolean disponivel;
-	private static String[] alf = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-			"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z" };
+	private String filas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+	/**
+	 * * Gera uma string Vaga com base nos parâmetros fila e numero.
+	 * *
+	 * * @param fila o parâmetro fila representa o número da fila.
+	 * * @param numero o parâmetro numero representa o número da vaga.
+	 * * @return uma string contendo o valor gerado da Vaga.
+	 * +
+	 */
 	public String Vaga(int fila, int numero) {
 		String letraVaga = "";
-		for (int i = 0; i < fila; i++) {
-			letraVaga = alf[fila].toString();
-		}
+		letraVaga += filas.charAt(fila - 1);
 		String vagaFinal = letraVaga + numero;
 		return vagaFinal;
 	}
 
+	/**
+	 * Valida se o carro pode ser estacionado na vaga 
+	 *
+	 * @return true se ele pode estacionar ou false se ele não pode estacionar
+	 */
 	public boolean estacionar() {
 		if (disponivel == false) {
 			return false;
@@ -22,6 +32,11 @@ public class Vaga {
 		}
 	}
 
+	/**
+	 * Verifica se a vaga esta disponivel e retorna se o usuario pode sair dela
+	 * 
+	 * @return true se ele puder sair ou false se a vaga estiver vazia
+	 */
 	public boolean sair() {
 		if (disponivel == false) {
 			return true;
@@ -30,6 +45,11 @@ public class Vaga {
 		}
 	}
 
+	/**
+	 * Verifica se esta disponivel
+	 *
+	 * @return true se estiver disponivel e false se não estiver disponivel
+	 */
 	public boolean disponivel() {
 		return disponivel;
 	}
