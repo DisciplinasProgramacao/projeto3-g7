718c01c3c7ca58c669237e51b2848b024138566f
Lucas Barros <lucas.barros@mindsdigital.net>
2023-10-17 20:12:45 -0300

    teste uso de vagas

--- codigo/test/TestUsoDeVaga.java
+++ codigo/test/TestUsoDeVaga.java
@@ -2,44 +2,38 @@ package test;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import codigo.UsoDeVaga;
 import codigo.Vaga;
-import codigo.Veiculo;
+
+import static org.junit.Assert.*;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 public class TestUsoDeVaga {
 
+    private UsoDeVaga usoDeVaga;
     private Vaga vaga;
-    private Veiculo veiculo;
-    private LocalDateTime entrada;
-    private LocalDateTime saida;
-    private double VALOR_FRACAO = 10;
-    private double VALOR_MAXIMO = 50;
-    private double valorPago;
 
     @Before
     public void setup() {
-        UsoDeVaga vagas = new UsoDeVaga(vaga);
-        veiculo = new Veiculo(null);
-        entrada = LocalDateTime.now();
-        saida = entrada.plusHours(1);
-        valorPago = 0;
+        vaga = new Vaga();
+        usoDeVaga = new UsoDeVaga(vaga);
     }
 
     @Test
-    public void testSairSuccess() {
-        UsoDeVaga vagas = new UsoDeVaga(vaga);
-        vagas.sair();
-        double expected = 10;
+    public void testGetValorPagoShouldReturnZeroIfVagaIsEmpty() {
+        double result = usoDeVaga.getValorPago();
+        assertEquals(0.0, result, 0.01);
+    }
 
-        double result = veiculo.sair();
+    @Test
+    public void testGetValorPagoShouldReturnValorPago() {
+        vaga.disponivel();
 
-        assertEquals(expected, result, 0);
-        assertEquals(saida, veiculo.sair());
+        double valorPago = usoDeVaga.sair();
+        double result = usoDeVaga.getValorPago();
+        assertEquals(valorPago, result, 0.01);
     }
 
 }
