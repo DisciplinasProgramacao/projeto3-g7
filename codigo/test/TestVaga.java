ff399217b4af49e5600a62da05aa51cd586ab652
Sérgio Parreiras <125428276+MaveHero@users.noreply.github.com>
2023-11-02 01:48:42 -0300

    Atulização do Teste da Classe Vaga #26
    #26

--- codigo/test/TestVaga.java
+++ codigo/test/TestVaga.java
@@ -4,66 +4,45 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import entities.Vaga;
 
-/**
- * Esta classe contém os testes unitários para a classe Vaga.
- */
 public class VagaTest {
 
-    /**
-     * Teste para o método estacionar() quando a vaga está disponível.
-     */
     @Test
     public void testEstacionarVagaDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        assertTrue(vaga.estacionar()); // Deve retornar true, pois a vaga está disponível
+        Vaga vaga = new Vaga(1, 1);
+        assertTrue(vaga.estacionar());
     }
 
-    /**
-     * Teste para o método estacionar() quando a vaga não está disponível.
-     */
     @Test
     public void testEstacionarVagaNaoDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        vaga.disponivel = false; // Defina a vaga como não disponível
-        assertFalse(vaga.estacionar()); // Deve retornar false, pois a vaga não está disponível
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertFalse(vaga.estacionar());
     }
 
-    /**
-     * Teste para o método sair() quando a vaga está disponível.
-     */
     @Test
     public void testSairVagaDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        assertTrue(vaga.sair()); // Deve retornar true, pois a vaga está disponível
+        Vaga vaga = new Vaga(1, 1);
+        assertTrue(vaga.sair());
     }
 
-    /**
-     * Teste para o método sair() quando a vaga não está disponível.
-     */
     @Test
     public void testSairVagaNaoDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        vaga.disponivel = false; // Defina a vaga como não disponível
-        assertTrue(vaga.sair()); // Deve retornar true, pois a vaga está disponível
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertTrue(vaga.sair());
     }
 
-    /**
-     * Teste para o método disponivel() quando a vaga está disponível.
-     */
     @Test
     public void testDisponivelVagaDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        vaga.disponivel = true; // Defina a vaga como disponível
-        assertTrue(vaga.disponivel()); // Deve retornar true, pois a vaga está disponível
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = true;
+        assertTrue(vaga.disponivel());
     }
 
-    /**
-     * Teste para o método disponivel() quando a vaga não está disponível.
-     */
     @Test
     public void testDisponivelVagaNaoDisponivel() {
-        Vaga vaga = new Vaga(1, 1); // Crie uma vaga
-        vaga.disponivel = false; // Defina a vaga como não disponível
-        assertFalse(vaga.disponivel()); // Deve retornar false, pois a vaga não está disponível
+        Vaga vaga = new Vaga(1, 1);
+        vaga.disponivel = false;
+        assertFalse(vaga.disponivel());
     }
 }
