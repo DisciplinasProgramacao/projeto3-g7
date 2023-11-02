819298b59a9f3ff08093be54b586792a1b0c5181
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-11-02 16:29:06 -0300

    atualizacaoUsoDeVaga

--- codigo/codigo/entities/entities/UsoDeVaga.java
+++ codigo/codigo/entities/entities/UsoDeVaga.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 public class UsoDeVaga {
 
+	private static final double FRACAO_USO = 0.25;
 	private static final double VALOR_FRACAO = 4.0;
 	private static final double VALOR_MAXIMO = 50.0;
 	private double valorServicos;
@@ -11,16 +12,19 @@ public class UsoDeVaga {
 	private LocalDateTime entrada;
 	private LocalDateTime saida;
 	private double valorPago;
-	private Servicos servicos;
+	private Servicos servico;
 
 	/**
 	 * Construtor da classe UsoDeVaga.
 	 *
 	 * @param vaga A vaga de estacionamento a ser usada.
 	 */
-	public UsoDeVaga(Vaga vaga) {
+	public UsoDeVaga(Vaga vaga, Servicos servicos) {
 		this.vaga = vaga;
-		this.entrada = LocalDateTime.now();
+		this.entrada = LocalDateTime.now();	
+		if(servico != null) {
+			this.servico = servico;
+		}
 	}
 
 	/**
@@ -30,23 +34,28 @@ public class UsoDeVaga {
 	 * @return O valor a ser pago pelo uso da vaga.
 	 */
 	public double sair() {
-		if (vaga.sair()) {
-			this.saida = LocalDateTime.now();
-			// Calcule o tempo de uso em horas
-			long tempoEmHoras = entrada.until(saida, java.time.temporal.ChronoUnit.HOURS);
-			if (tempoEmHoras >= servicos.getMinPermanencia()) {
-				valorServicos = servicos.getValor();
-			} else {
-				return 0;
-			}
-			// Calcule o valor a ser pago, limitado ao valor máximo
-			valorPago = (Math.min(tempoEmHoras * VALOR_FRACAO, VALOR_MAXIMO)) + valorServicos;
+        this.saida = LocalDateTime.now();
+        long minutosEstacionado = calcularMinutosEstacionado();
+        double valorPago = calcularValorPago(minutosEstacionado);
+        return valorPago;
+    }
 
-			return valorPago;
-		} else {
-			return 0;
+	private long calcularMinutosEstacionado() {
+        return entrada.until(saida, java.time.temporal.ChronoUnit.MINUTES);
+    }
+
+	private double calcularValorPago(long minutosEstacionado) {
+		double valor = (minutosEstacionado * FRACAO_USO * VALOR_FRACAO);
+		if(valor > VALOR_MAXIMO){
+			valor = VALOR_MAXIMO;
 		}
-	}
+
+		if (servico != null) {
+			return servico.getValor();
+		} else {
+			return 0.0; // ou algum outro valor padrão se servico for nulo
+		}
+		}
 
 	/**
 	 * Verifica se o uso da vaga ocorreu no mês especificado.
@@ -69,9 +78,9 @@ public class UsoDeVaga {
 	}
 
 	public Servicos adicionarServico(Servicos servicos) {
-		if (this.servicos == null) {
-			this.servicos = servicos;
-			return this.servicos;
+		if (this.servico == null) {
+			this.servico = servicos;
+			return this.servico;
 		} else {
 			return servicos;
 		}
