58eac080041efb88b405c0ab4edccfb5c2b92501
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-11-20 15:48:05 -0300

    usoDeVagaUpdate

--- codigo/codigo/entities/entities/Servicos.java
+++ codigo/codigo/entities/entities/Servicos.java
@@ -1,39 +1,33 @@
 package entities;
-
 public enum Servicos {
-    MANOBRISTA(5.0, "manobrista", 0.0),
-    LAVAGEM(20.0, "lavagem", 1.0),
-    POLIMENTO(45.0, "polimento", 2.0);
 
-    private double valor;
-    private String descricao;
-    private double minPermanencia;
+    MANOBRISTA("Manobrista",5.0,0),
+    LAVAGEM("Lavagem",20.0,60),
+    POLIMENTO("Polimento",45.0, 0);
 
-    private Servicos(double valor, String descricao, double minPermanencia) {
+
+    private String nome;
+    private Double valor;
+    private int tempo;
+
+    Servicos(String nome, double valor, int tempo){
+
+        this.nome= nome;
         this.valor = valor;
-        this.descricao = descricao;
-        this.minPermanencia = minPermanencia;
+        this.tempo = tempo;   
     }
 
-    public double getValor() {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+    
+    public Double getValor() {
         return valor;
     }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public double getMinPermanencia() {
-        return minPermanencia;
-    }
-
-    public double calculateValue(double tempoPermanencia) {
-        double valorAdicional = 0.0;
-
-        if (tempoPermanencia < minPermanencia) {
-            valorAdicional = (minPermanencia - tempoPermanencia) * 10.0;
-        }
-
-        return valor + valorAdicional;
-    }
+  
 }
--- codigo/codigo/entities/entities/UsoDeVaga.java
+++ codigo/codigo/entities/entities/UsoDeVaga.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class UsoDeVaga {
 
@@ -11,86 +13,45 @@ public class UsoDeVaga {
 	private LocalDateTime entrada;
 	private LocalDateTime saida;
 	private double valorPago;
-	private Servicos servico;
-	private boolean saiu; 
+	private Servicos servicos;
+	private boolean saiu;
 
-	/**
-	 * Construtor da classe UsoDeVaga.
-	 *
-	 * @param vaga A vaga de estacionamento a ser usada.
-	 */
+	
 	public UsoDeVaga(Vaga vaga) {
 		this.vaga = vaga;
 		this.entrada = LocalDateTime.now();
-		this.saida = null; // Inicializa a saída como nula
-		this.saiu = false; // Inicializa o status de saída como falso
-
-		
 	}
 
-	/**
-	 * Registra a saída do veículo da vaga e calcula o valor a ser pago com base no
-	 * tempo de uso.
-	 *
-	 * @return O valor a ser pago pelo uso da vaga.
-	 */
 	public double sair() {
-		if (saiu) {
-			throw new IllegalStateException("O veículo já saiu da vaga.");
-		}
-
 		this.saida = LocalDateTime.now();
-		long minutosEstacionado = calcularMinutosEstacionado();
-		double valorPago = calcularValorPago(minutosEstacionado);
-
-		saiu = true; // Define o status de saída como verdadeiro
-
-		return valorPago;
+        int tempoPermanenciaMinutos = (int) entrada.until(saida, ChronoUnit.MINUTES);
+        if (servicos != null) {
+            if (tempoPermanenciaMinutos >= servicos.getTempo()) {
+                return valorPago() + servicos.getValor();
+            } else {
+                return valorPago();
+            }
+        }
+        return valorPago();
 	}
 
-	private long calcularMinutosEstacionado() {
-		return entrada.until(saida, java.time.temporal.ChronoUnit.MINUTES);
-	}
-
-	private double calcularValorPago(long minutosEstacionado) {
-		double valor = (minutosEstacionado * FRACAO_USO * VALOR_FRACAO);
-		if (valor > VALOR_MAXIMO) {
-			valor = VALOR_MAXIMO;
-		}
-
-		if (servico != null) {
-			return servico.getValor();
-		} else {
-			return 0.0; // ou algum outro valor padrão se servico for nulo
-		}
-	}
-
-	/**
-	 * Verifica se o uso da vaga ocorreu no mês especificado.
-	 *
-	 * @param mes O mês a ser verificado (1 para janeiro, 2 para fevereiro, etc.).
-	 * @return true se o uso da vaga ocorreu no mês especificado, false caso
-	 *         contrário.
-	 */
 	public boolean ehDoMes(int mes) {
 		return this.entrada.getMonthValue() == mes;
 	}
 
-	/**
-	 * Obtém o valor pago pelo uso da vaga.
-	 *
-	 * @return O valor pago pelo uso da vaga.
-	 */
 	public double valorPago() {
-		return valorPago;
-	}
+		int calcTempo = (int) entrada.until(saida, ChronoUnit.MINUTES);
+        if (calcTempo == 0)
+            calcTempo = 1;
+        int quantidadeFracoesTempo = (int) Math.ceil(calcTempo / 15.0);
+        valorPago = quantidadeFracoesTempo * VALOR_FRACAO;
 
-	public Servicos adicionarServico(Servicos servicos) {
-		if (this.servico == null) {
-			this.servico = servicos;
-			return this.servico;
-		} else {
-			return servicos;
-		}
-	}
+        if (valorPago > VALOR_MAXIMO) valorPago = VALOR_MAXIMO;
+
+        return valorPago;
+    }
+
+    public void contratarServico(Servicos servico) {
+        this.servicos = servico;
+    }
 }
