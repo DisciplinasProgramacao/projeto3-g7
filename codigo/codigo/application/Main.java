6301bb075331be694e247e29be300b01c8d7a78c
MaveHero <sergioparreiras09@gmail.com>
2023-11-22 15:05:44 -0300

    fix: fixing main and adding cliente horista methods

--- codigo/codigo/application/Main.java
+++ codigo/codigo/application/Main.java
@@ -1,114 +1,166 @@
+
 import java.util.Scanner;
 import entities.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); 
-        Estacionamento estacionamento = new Estacionamento("Estacionamento", 3,5);
+        Scanner sc = new Scanner(System.in);
+        Estacionamento estacionamento = new Estacionamento("Estacionamento", 3, 5);
+        MenuHandler menuHandler = new MenuHandler(sc, estacionamento);
+        menuHandler.runMenu();
+        sc.close();
+    }
+}
+
+class MenuHandler {
+    private Scanner scanner;
+    private Estacionamento estacionamento;
+
+    public MenuHandler(Scanner scanner, Estacionamento estacionamento) {
+        this.scanner = scanner;
+        this.estacionamento = estacionamento;
+    }
+
+    public void runMenu() {
         boolean sair = false;
 
-
         while (!sair) {
-            System.out.println("===== MENU DO ESTACIONAMENTO =====");
-            System.out.println("1. Registrar Cliente");
-            System.out.println("2. Registrar Veiculo");
-            System.out.println("3. Estacionar carro");
-            System.out.println("4. Calcular valor de saida");
-            System.out.println("5. Total arrecadado do estacionamento");
-            System.out.println("6. Total arrecadado do estacionamento em um mes");
-            System.out.println("7. Media de gasto do publico");
-            System.out.println("8. Top 5 clientes do estacionamento ");
-            System.out.println("9. Sair");
-            System.out.print("Escolha uma opção: ");
-            int opcao = sc.nextInt();
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("REGISTRAR CLIENTE");
-                    System.out.print("Nome do cliente?: ");
-                    sc.nextLine();
-                    String nome = sc.nextLine();
-                    System.out.print("ID do cliente: ");
-                    String id = sc.nextLine();
-                    Cliente cliente = new Cliente(nome, id);
-                    estacionamento.addCliente(cliente);
+                    registrarCliente();
                     break;
                 case 2:
-                    System.out.print("Placa do Veiculo: ");
-                    String placa = sc.nextLine();
-                    sc.nextLine();
-                    Veiculo veiculo = new Veiculo(placa);
-                    System.out.print("ID DO CLIENTE: ");
-                    String idCliente = sc.nextLine();
-                    estacionamento.addVeiculo(veiculo, idCliente);
+                    registrarVeiculo();
                     break;
                 case 3:
-                    System.out.print("Placa do carro: ");
-                    String placaVeiculo = sc.nextLine();
-                    sc.nextLine();
-                    estacionamento.estacionar(placaVeiculo);
+                    estacionarCarro();
                     break;
                 case 4:
-                    System.out.println("Digite a placa do carro:");
-                    String placaVaga = sc.nextLine();
-                    sc.nextLine();
-                    System.out.print("Valor a ser pago: " + estacionamento.sair(placaVaga));
+                    calcularValorSaida();
                     break;
                 case 5:
-                    System.out.println("Total arrecadado: " + estacionamento.totalArrecadado());
+                    exibirTotalArrecadado();
                     break;
                 case 6:
-                    System.out.println("Qual mes voce deseja?: ");
-                    int mes = sc.nextInt();
-                    System.out.print("Total arrecadado: " + estacionamento.arrecadacaoNoMes(mes));
+                    totalArrecadadoMes();
                     break;
                 case 7:
-                    System.out.println("Gastos medios: " + estacionamento.valorMedioPorUso());
+                    exibirGastosMedios();
+                    break;
+                case 8:
+                    totalArrecadadoMesClienteHorista();
                     break;
                 case 9:
-                    sair = false;
+                    exibirTotalArrecadadoClienteHorista();
+                    break;
+                case 10:
+                    sair = true;
                     break;
                 default:
-                    System.out.println("Opcao invalida. Tente novamente.");
+                    System.out.println("Escolha incorreta. Tente novamente.");
                     break;
             }
         }
-
-        sc.close();
     }
 
-    // Gerar aleatoriamente um mínimo de 50 usos de vagas distribuídas para clientes
-    // e estacionamentos;
-     public void carregarDados() {
-        Estacionamento[] estacionamentos = new Estacionamento[3];
-        estacionamentos[0] = new Estacionamento("Estacionamento1", 4, 5);
-        estacionamentos[1] = new Estacionamento("Estacionamento2", 5, 6);
-        estacionamentos[2] = new Estacionamento("Estacionamento3", 4, 5);
-    
-        Cliente[] clientes = new Cliente[3];
-        clientes[0] = new Cliente("Cliente1", "1");
-        clientes[1] = new Cliente("Cliente2", "2");
-        clientes[2] = new Cliente("Cliente3", "3");
-    
-        Veiculo[] veiculos = new Veiculo[3];
-        veiculos[0] = new Veiculo("1");
-        veiculos[1] = new Veiculo("2");
-        veiculos[2] = new Veiculo("3");
-    
-        for (Estacionamento estacionamento : estacionamentos) {
-            for (Cliente cliente : clientes) {
-                estacionamento.addCliente(cliente);
-            }
-        }
-        for (int i = 0; i < veiculos.length; i++) {
-            Cliente cliente = clientes[i];
-            Veiculo veiculo = veiculos[i];
-            cliente.addVeiculo(veiculo);
-        }
+    private void exibirMenu() {
+        System.out.println("===== MENU DO ESTACIONAMENTO =====");
+        System.out.println("1. Registrar Cliente");
+        System.out.println("2. Registrar Veiculo");
+        System.out.println("3. Estacionar carro");
+        System.out.println("4. Calcular valor de saida");
+        System.out.println("5. Total arrecadado do estacionamento");
+        System.out.println("6. Total arrecadado do estacionamento em um mes");
+        System.out.println("7. Media de gasto do publico");
+        System.out.println("8. Top 5 clientes do estacionamento ");
+        System.out.println("8. Total arrecadado do Cliente Horista; ");
+        System.out.println("9. Sair");
+        System.out.print("Escolha uma opção: ");
     }
 
-    
-    
-    
+    private void registrarCliente() {
+        System.out.println("===== REGISTRO DE CLIENTE =====");
+        System.out.print("Nome do cliente: ");
+        scanner.nextLine();
+        String nome = scanner.nextLine();
+        System.out.print("ID do cliente: ");
+        String id = scanner.nextLine();
+        Cliente cliente = new Cliente(nome, id);
+        estacionamento.addCliente(cliente);
+    }
+
+    private void registrarVeiculo() {
+        System.out.println("===== REGISTRO DE VEICULO =====");
+        System.out.print("Placa do Veiculo: ");
+        String placa = scanner.nextLine();
+        scanner.nextLine();
+        Veiculo veiculo = new Veiculo(placa);
+        System.out.print("ID DO CLIENTE: ");
+        String idCliente = scanner.nextLine();
+        estacionamento.addVeiculo(veiculo, idCliente);
+    }
+
+    private void estacionarCarro() {
+        System.out.println("===== ESTACIONANDO VEICULO =====");
+        System.out.print("Placa do carro: ");
+        String placaVeiculo = scanner.nextLine();
+        scanner.nextLine();
+        estacionamento.estacionar(placaVeiculo);
+        System.out.println("Veiculo estacionado com sucesso!");
+    }
+
+    private void calcularValorSaida() {
+        System.out.println("===== CALCULANDO SAIDA VEICULO =====");
+        System.out.println("Digite a placa do carro: ");
+        String placaVaga = scanner.nextLine();
+        scanner.nextLine();
+        System.out.print("Valor a ser pago: " + estacionamento.sair(placaVaga));
+    }
+
+    private void exibirTotalArrecadado() {
+        System.out.println("===== TOTAL ARRECADADO DO ESTACIONAMENTO =====");
+        double totalArrecadado = estacionamento.totalArrecadado();
+        System.out.println("Total arrecadado: " + totalArrecadado);
+    }
+
+    private void totalArrecadadoMes() {
+        System.out.println("===== TOTAL ARRECADADO DO ESTACIONAMENTO EM UM MES =====");
+        System.out.println("Qual mes voce deseja? ");
+        int mes = scanner.nextInt();
+        System.out.print("Total arrecadado: " + estacionamento.arrecadacaoNoMes(mes));
+    }
+
+    private void exibirGastosMedios() {
+        System.out.println("===== MEDIA DE GASTOS DO PUBLICO =====");
+        System.out.println("Gastos medios: " + estacionamento.valorMedioPorUso());
+    }
+
+    private void totalArrecadadoMesClienteHorista() {
+        System.out.println("===== TOTAL ARRECADADO DO ESTACIONAMENTO EM UM MES CLIENTE HORISTA =====");
+        System.out.println("Qual mes voce deseja? ");
+        int mes = scanner.nextInt();
+        System.out.print("Total arrecadado: " + estacionamento.arrecadacaoNoMesClienteHorista(mes));
+    }
+
+    private void exibirTotalArrecadadoClienteHorista() {
+        System.out.println("===== TOTAL ARRECADADO DO ESTACIONAMENTO =====");
+        double totalArrecadadoClienteHorista = estacionamento.totalArrecadadoHorista();
+        System.out.println("Total arrecadado: " + totalArrecadadoClienteHorista);
+    }
+
+    // Dudão, arruma isso. O método de exibir top 5 não está funcionando. Não pode
+    // ter get tbm, se tiver get, tá errado.
+    // Mas o caminho dele já ta certinho no main, é só arrumar, e tirar o comentário
+    // q ele vai chamar certinho.
+    // private void exibirTop5() {
+    // System.out.println("===== TOP 5 CLIENTES DO ESTACIONAMENTO =====");
+    // List<Cliente> top5 = estacionamento.top5();
+    // for (Cliente cliente : top5) {
+    // System.out.println(cliente.toString());
+    // }
+    // }
 }
-
