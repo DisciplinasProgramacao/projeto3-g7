c27db3187b67950b62ea5721a6b5c6349b486f8d
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-12-02 14:18:23 -0300

    UsoDeVagaHoristaFactoryAndInterfaceUsoDeVagaFactory

---
+++ codigo/codigo/entities/entities/interfaces/UsoDeVagaFactory.java
@@ -0,0 +1,27 @@
+package entities.interfaces;
+
+import entities.UsoDeVaga;
+import entities.UsoDeVagaHoristaFactory;
+import entities.Vaga;
+
+public interface UsoDeVagaFactory {
+
+    /**
+     * Cria um uso de vaga para a vaga passada como parâmetro.
+     * 
+     * @param vaga Vaga para a qual o uso de vaga será criado.
+     * @return Uso de vaga criado.
+     */
+    public UsoDeVaga criarUsoDeVaga(Vaga vaga);
+
+    // Método estático para criar instâncias da fábrica
+
+    // Método estático para criar instâncias da fábrica
+    static UsoDeVagaFactory criarHoristaFactory() {
+        return new UsoDeVagaHoristaFactory();
+    }
+
+
+}
+
+
