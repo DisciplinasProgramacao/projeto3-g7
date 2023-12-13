d8885b343e2b68a7c6c1952985a301aa50869005
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-12-13 17:07:45 -0300

    testVeiculoAndGerarDadosSTART

--- codigo/test/TestVeiculo.java
+++ codigo/test/TestVeiculo.java
@@ -1,58 +1,51 @@
 package test;
 
+import entities.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import entities.Veiculo;
 import entities.Vaga;
 
 public class TestVeiculo {
+
     @Test
     public void testEstacionar() {
-        Veiculo veiculo = new Veiculo("ABC1234");
-        Vaga vaga = new Vaga(4, 7);
-        veiculo.estacionar(vaga);
-        assertEquals(0, veiculo.getIndiceDeVaga());
+        Veiculo veiculo = new Veiculo("ABC123");
+        Vaga vaga = new Vaga(1, 1);
+        veiculo.estacionar(vaga, null);
+        assertEquals(4.0, veiculo.sair(), 0.0);
     }
 
     @Test
     public void testSair() {
-        Veiculo veiculo = new Veiculo("ABC1234");
-        Vaga vaga = new Vaga(4, 7);
-        veiculo.estacionar(vaga);
-        double valorPago = veiculo.sair();
-        assertEquals(10.0, valorPago, 0.01);
-    }
-
-    @Test
-    public void testTotalArrecadado() {
-        Veiculo veiculo = new Veiculo("ABC1234");
-        Vaga vaga1 = new Vaga(4, 7);
-        Vaga vaga2 = new Vaga(5, 8);
-        veiculo.estacionar(vaga1);
-        veiculo.estacionar(vaga2);
-        double totalArrecadado = veiculo.totalArrecadado();
-        assertEquals(20.0, totalArrecadado, 0.01);
-    }
-
-    @Test
-    public void testArrecadadoNoMes() {
-        Veiculo veiculo = new Veiculo("ABC1234");
-        Vaga vaga1 = new Vaga(4, 7);
-        Vaga vaga2 = new Vaga(5, 8);
-        veiculo.estacionar(vaga1);
-        veiculo.estacionar(vaga2);
-        double arrecadadoNoMes = veiculo.arrecadadoNoMes(4);
-        assertEquals(10.0, arrecadadoNoMes, 0.01);
+        Veiculo veiculo = new Veiculo("ABC123");
+        Vaga vaga = new Vaga(1, 1);
+        veiculo.estacionar(vaga, null);
+        veiculo.sair();
+        assertNull(veiculo.sair());
     }
 
     @Test
     public void testTotalDeUsos() {
-        Veiculo veiculo = new Veiculo("ABC1234");
-        Vaga vaga1 = new Vaga(4, 7);
-        Vaga vaga2 = new Vaga(5, 8);
-        veiculo.estacionar(vaga1);
-        veiculo.estacionar(vaga2);
-        int totalDeUsos = veiculo.totalDeUsos();
-        assertEquals(1, totalDeUsos);
+        Veiculo veiculo = new Veiculo("ABC123");
+        Vaga vaga1 = new Vaga(1, 1);
+        Vaga vaga2 = new Vaga(2, 2);
+        veiculo.estacionar(vaga1, null);
+        veiculo.estacionar(vaga2, null);
+        assertEquals(2, veiculo.totalDeUsos());
+    } 
+
+    @Test
+    public void testTotalArrecadado() {
+        Veiculo veiculo = new Veiculo("ABC123");
+        Vaga vaga1 = new Vaga(1, 1);
+        Vaga vaga2 = new Vaga(2, 2);
+        veiculo.estacionar(vaga1, null);
+        veiculo.estacionar(vaga2, null);
+        assertEquals(20.0, veiculo.totalArrecadado(), 0.0);
     }
+
+   
 }
+
+    
