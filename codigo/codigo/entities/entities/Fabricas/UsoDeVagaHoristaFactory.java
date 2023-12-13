e9190d59c0492ac038b0b91ceffdce9a48294ea6
Lucas Barros <lucas.barros@mindsdigital.net>
2023-12-13 16:14:59 -0300

    CRIANDO FABRICAS

---
+++ codigo/codigo/entities/entities/Fabricas/UsoDeVagaHoristaFactory.java
@@ -0,0 +1,18 @@
+package entities.Fabricas;
+
+import entities.UsoDeVaga;
+import entities.UsoHorista;
+import entities.Vaga;
+import entities.interfaces.UsoDeVagaFactory;
+
+public class UsoDeVagaHoristaFactory implements UsoDeVagaFactory {
+    @Override
+    /**
+     * Cria um uso de vaga para a vaga passada como parâmetro.
+     * Nesse caso, é uma vaga de Horista
+     */
+    public UsoDeVaga criarUsoDeVaga(Vaga vaga) {
+        return new UsoHorista(vaga);
+    }
+
+}
